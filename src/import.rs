82518d1f0ac2b7918d9443695c8c595cd32abd41
Max Ammann <max.ammann@zoo.dev>
2026-05-07 16:42:03 +0200

    fix crahs with invalid UTF-8 (#7)

--- src/import.rs
+++ src/import.rs
@@ -47,7 +47,10 @@ impl<'a> Scheme<'a> {
                 Scheme::Unsupported
             }
         } else {
-            Scheme::Relative(urlencoding::decode(uri).unwrap())
+            match urlencoding::decode(uri) {
+                Ok(decoded) => Scheme::Relative(decoded),
+                Err(_) => Scheme::Unsupported,
+            }
         }
     }
 
